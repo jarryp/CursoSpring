2ef46bd215bed957751d3f222767e2e52a14fd04
Jarry Palacios <jarry.palacios@hotmail.com>
2019-06-09 23:26:20 -0400

    Corrigiendo llamado de URLs

--- src/main/java/com/palaciossystems/core/ControllerBasic.java
+++ src/main/java/com/palaciossystems/core/ControllerBasic.java
@@ -1,14 +1,20 @@
-package controllers;
+package com.palaciossystems.core;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 @RequestMapping("/hola")
 public class ControllerBasic {
 	
-	@RequestMapping(path = {"/saludar","/holamundo"})
+	@GetMapping(path = {"/saludar","/holamundo"})
 	public String saludar() {
 		return "index";
+	}
+	
+	@GetMapping(path = {"/saluda"})
+	public String saluda() {
+		return "saludo";
 	}
 }
--- src/main/java/com/palaciossystems/core/CursoSpringApplication.java
+++ src/main/java/com/palaciossystems/core/CursoSpringApplication.java
@@ -1,9 +1,13 @@
 package com.palaciossystems.core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan()
 public class CursoSpringApplication {
 
 	public static void main(String[] args) {
